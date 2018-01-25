015a9c0d8e9077f266043b1f51fe8daae7555697
Dillon Titcomb <dillontitcomb@gmail.com>
2018-01-25 10:58:30 -0800

    Adds function that creates movie Ticket with variables name, age, movie, and movie time.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
+inputName = "";
+inputAge = "";
+inputTitle = "";
+inputTime = "";
+ticketPrice = 10;
+
+function gatherInfo() {
+  inputName = $("input#name").val();
+  inputAge = $("input#age").val();
+  inputTitle = $("#movie-title").val();
+  inputTime = $("#movie-time").val();
+}
+
+function Ticket(name, age, title, time) {
+  this.userName = name;
+  this.userAge = age;
+  this.movieTitle = title;
+  this.movieTime = time;
+}
+
+
 $(document).ready(function() {
   $("form#movie-details").submit(function(event) {
-      alert("hi");
-      event.preventDefault();
+    event.preventDefault();
+    gatherInfo();
+    var newTicket = new Ticket(inputName, inputAge, inputTitle, inputTime);
+    alert(newTicket.userName);
     });
 });
